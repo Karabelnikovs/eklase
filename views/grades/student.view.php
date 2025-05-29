3caf57b9e83fc5b0227b0a50d2f59439696b581f
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2025-05-29 13:42:42 +0300

    student views

--- views/grades/student.view.php
+++ views/grades/student.view.php
@@ -1,22 +1,25 @@
 <?php $title = "My Grades"; ?>
-<h1 class="text-3xl font-bold text-center mb-6">My Grades</h1>
 <div class="container mx-auto">
-    <table class="w-full text-left border-collapse">
-        <thead>
-            <tr class="bg-gray-200">
-                <th class="p-2">Subject</th>
-                <th class="p-2">Grade</th>
-                <th class="p-2">Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($grades as $grade): ?>
-                <tr>
-                    <td class="p-2 border"><?= htmlspecialchars($grade['subject_name']) ?></td>
-                    <td class="p-2 border"><?= htmlspecialchars($grade['grade']) ?></td>
-                    <td class="p-2 border"><?= htmlspecialchars($grade['created_at']) ?></td>
+    <h1 class="text-3xl font-bold text-center mb-6 text-white">My Grades</h1>
+    <div class="container mx-auto">
+
+        <table class="w-full text-left border-collapse" id="gradesTable">
+            <thead>
+                <tr class="bg-gray-100 text-gray-700">
+                    <th class="p-4 text-sm font-semibold tracking-wide">Subject</th>
+                    <th class="p-4 text-sm font-semibold tracking-wide">Grade</th>
+                    <th class="p-4 text-sm font-semibold tracking-wide">Date</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($grades as $grade): ?>
+                    <tr class="border-b  transition-colors">
+                        <td class="p-4 text-white"><?= htmlspecialchars($grade['subject_name']) ?></td>
+                        <td class="p-4 text-white"><?= htmlspecialchars($grade['grade']) ?></td>
+                        <td class="p-4 text-white"><?= htmlspecialchars($grade['created_at']) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
